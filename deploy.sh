278ba939e1eff39841ca3bf0ef259b3135523b40
Alin Panaitiu <alin.panaitiu@gmail.com>
2021-04-17 19:10:55 +0300

    Fixes

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ TAG=$TRAVIS_COMMIT docker-compose push
 
 if [[ "$TRAVIS_BRANCH" = "master" || "$TRAVIS_BRANCH" = "main" ]]; then
     docker-compose push
-    curl --basic --user alin:$DEPLOY_PASSWORD --get -d "update=$UPDATE_COMMAND" -d "restart=$RESTART" https://deploy.darkwoods.win/$STACK
+    curl --basic --user alin:$DEPLOY_PASSWORD --get --data-urlencode "update=$UPDATE_COMMAND" --data-urlencode "restart=$RESTART" --data-urlencode "async=$ASYNC" https://deploy.darkwoods.win/$STACK
 fi
