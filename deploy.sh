ab6a128fb8ec59737576e1db34bde521b64d2d0e
Alin Panaitiu <alin.panaitiu@gmail.com>
2021-05-11 10:57:31 +0300

    Matterllo

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "$DOCKER_PASSWORD" | docker login -u alinpanaitiu --password-stdin
-
-TAG=$TRAVIS_COMMIT docker-compose push
-
-if [[ "$TRAVIS_BRANCH" = "master" || "$TRAVIS_BRANCH" = "main" ]]; then
-    docker-compose push
-    curl --basic --user alin:$DEPLOY_PASSWORD --get --data-urlencode "update=$UPDATE_COMMAND" --data-urlencode "restart=$RESTART" --data-urlencode "async=$ASYNC" https://deploy.darkwoods.win/$STACK
-fi
